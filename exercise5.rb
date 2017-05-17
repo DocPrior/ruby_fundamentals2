7571fba95514902a608062b2065b5a5570c44ced
DocPrior <Sara Prior>
2017-05-17 13:12:25 -0400

    exercise 5 method works

---
+++ exercise5.rb
@@ -0,0 +1,12 @@
+def greet_backwards(name)
+  backward = name.reverse
+  return backward
+end
+
+puts greet_backwards('Sara')
+
+puts greet_backwards('Bob')
+
+puts greet_backwards('Shirly')
+
+puts greet_backwards('Andy')
