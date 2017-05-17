cbd13b919c644cb36f3b9e726a678b4644863e6b
DocPrior <Sara Prior>
2017-05-17 14:36:31 -0400

    finish 9.2

--- exercise9.rb
+++ exercise9.rb
@@ -1,5 +1,15 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-grocery_list.each do |item|
- puts "* #{item}"
+def grocery_list_list(arr)
+  arr.each do |item|
+   puts "* #{item}"
+  end
 end
+
+def grocery_add(arr, new_item)
+  arr.push(new_item)
+end
+
+grocery_add(grocery_list, "rice")
+
+grocery_list_list(grocery_list)
