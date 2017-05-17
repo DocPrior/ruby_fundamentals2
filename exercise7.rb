6cfd5eeaf7f26fb92dd24cd512b4a2abce02e409
DocPrior <Sara Prior>
2017-05-17 13:54:49 -0400

    finish exercise 7

--- exercise7.rb
+++ exercise7.rb
@@ -1,3 +1,10 @@
 def wrap_text(words, wrapper)
-  words.concat(wrapper)
+  wordcat_1 = words.concat(wrapper)
+  wordcat_2 = wrapper.concat(wordcat_1)
 end
+
+new_word_1 = wrap_text("hello", "###")
+
+new_word_2 = wrap_text(new_word_1, "===" )
+
+puts wrap_text(new_word_2, "---")
